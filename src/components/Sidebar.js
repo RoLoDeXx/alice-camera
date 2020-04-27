ee115d0c913f0749fd44902e0f35f50f86056f08
Samarth Sharma <dexxrolo@gmail.com>
2020-04-28 00:49:50 +0530

    sideboard

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,47 @@
 import React from "react";
 import logo from "../assets/images/logo.jpg";
+import { Link } from "react-router-dom";
+
+let stash = [
+  {
+    icon: "home",
+    title: "Dashboard",
+    link: "#",
+  },
+  {
+    icon: "user",
+    title: "People",
+    link: "/people",
+  },
+  {
+    icon: "bullhorn",
+    title: "Campaigns",
+    link: "/camps",
+  },
+  {
+    icon: "file-alt",
+    title: "Templates",
+    link: "",
+  },
+  {
+    icon: "cog",
+    title: "Settings",
+    link: "",
+  },
+];
 
 export default function Sidebar() {
   return (
-    <div className="sidebar ">
+    <div className="sidebar">
       <img src={logo} className="w-100 " alt="site-logo" />
-      <div className="container-fluid d-flex justify-content-between align-items-baseline">
-        <span>el</span>
-        <p>Dashboard</p>
+      <div className="container mt-4">
+        {stash.map((item) => (
+          <div className="sidebar-grid">
+            <i className={`fas fa-${item.icon}`}></i>
+            <Link to={item.link}> {item.title}</Link>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
-const sideBarList = (font, title, route = "#") => {};
