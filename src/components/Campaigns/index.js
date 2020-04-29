375d0fcad0f0d7d63a0e825c62f148bc75716fcb
Samarth Sharma <dexxrolo@gmail.com>
2020-04-29 06:10:37 +0530

    initial stepper

--- src/components/Campaigns/index.js
+++ src/components/Campaigns/index.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function index() {
   return (
-    <div>
-      #3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff#3e88ff
+    <div className="stepper">
+      <li className="stepper__item complete">Step 1</li>
+      <li className="stepper__item current">Step 2</li>
+      <li className="stepper__item">Step 3</li>
+      <li className="stepper__item">Step 4</li>
+      <li className="stepper__item">Step 5</li>
     </div>
   );
 }
