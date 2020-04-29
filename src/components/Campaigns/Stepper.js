d1392c5c8faba4ed0d88b5814b6c5c92849d0a84
Samarth Sharma <dexxrolo@gmail.com>
2020-04-29 18:41:43 +0530

    onclick stepper working

--- src/components/Campaigns/Stepper.js
+++ src/components/Campaigns/Stepper.js
@@ -7,11 +7,23 @@ export default class Stepper extends Component {
       gb: 1,
     };
   }
+
+  jumpToStep = (number) => {
+    this.setState({
+      gb: number,
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="stepper">
-          <span className="stepper-item complete d-flex align-items-center justify-content-around">
+          <span
+            className="stepper-item complete d-flex align-items-center justify-content-around"
+            onClick={() => {
+              this.jumpToStep(1);
+            }}
+          >
             {this.state.gb > 1 ? (
               <span className="active-tick">
                 <i className="fas fa-check"></i>
@@ -21,8 +33,14 @@ export default class Stepper extends Component {
             )}
             <span>Sequence</span>
           </span>
+
           {this.state.gb >= 2 ? (
-            <span className="stepper-item complete d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item complete d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(2);
+              }}
+            >
               {this.state.gb > 2 ? (
                 <span className="active-tick">
                   <i className="fas fa-check"></i>
@@ -33,13 +51,23 @@ export default class Stepper extends Component {
               <span>Schedule</span>
             </span>
           ) : (
-            <span className="stepper-item d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(2);
+              }}
+            >
               <span className="unactive-circle">2</span> <span>Schedule</span>
             </span>
           )}
 
           {this.state.gb >= 3 ? (
-            <span className="stepper-item complete d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item complete d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(3);
+              }}
+            >
               {this.state.gb > 3 ? (
                 <span className="active-tick">
                   <i className="fas fa-check"></i>
@@ -50,12 +78,22 @@ export default class Stepper extends Component {
               <span>People</span>
             </span>
           ) : (
-            <span className="stepper-item d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(3);
+              }}
+            >
               <span className="unactive-circle">3</span> <span>People</span>
             </span>
           )}
           {this.state.gb >= 4 ? (
-            <span className="stepper-item complete d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item complete d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(4);
+              }}
+            >
               {this.state.gb > 4 ? (
                 <span className="active-tick">
                   <i className="fas fa-check"></i>
@@ -66,12 +104,22 @@ export default class Stepper extends Component {
               <span>Settings</span>
             </span>
           ) : (
-            <span className="stepper-item d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(4);
+              }}
+            >
               <span className="unactive-circle">4</span> <span>Settings</span>
             </span>
           )}
           {this.state.gb >= 5 ? (
-            <span className="stepper-item stepper-ender complete d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item stepper-ender complete d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(5);
+              }}
+            >
               {this.state.gb > 5 ? (
                 <span className="unactive-circle">5</span>
               ) : (
@@ -80,7 +128,12 @@ export default class Stepper extends Component {
               <span>Review</span>
             </span>
           ) : (
-            <span className="stepper-item d-flex align-items-center justify-content-around">
+            <span
+              className="stepper-item d-flex align-items-center justify-content-around"
+              onClick={() => {
+                this.jumpToStep(5);
+              }}
+            >
               <span className="unactive-circle">5</span> <span>Review</span>
             </span>
           )}
